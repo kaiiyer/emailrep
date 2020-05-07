a1d4c5b7fdeccc645b54869cf2d4f1df8ffdfba5
kaiiyer <kaiiyer47@yahoo.in>
2020-05-07 21:47:42 +0530

    Minor Fixes

---
+++ email_test.go
@@ -0,0 +1,47 @@
+package emailrep
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type people struct {
+	Number int `json:"number"`
+}
+
+type GetWebRequest interface {
+	FetchBytes(url string) []byte
+}
+
+type LiveGetWebRequest struct {
+}
+
+func (LiveGetWebRequest) FetchBytes(url string) []byte {
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "spacecount-tutorial")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+	if body != nil {
+		defer body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	return body
+}
