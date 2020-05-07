6c397a079d82f5611d72682eea01cf92a78f8934
kaiiyer <kaiiyer47@yahoo.in>
2020-05-07 16:52:19 +0530

    Minor Fixes

---
+++ email.go
@@ -0,0 +1,26 @@
+package emailrep
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func emailrep(email string) string {
+	/* local variable definition */
+
+	var mail = email
+
+	//	resp, err := http.Get("https://emailrep.io/" + mail)
+
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1" + mail)
+	body, _ := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handling err
+		fmt.Printf("Sorry we got hit by an error!\n")
+	}
+	defer resp.Body.Close()
+	fmt.Println(string(body))
+
+	return string(body)
+}
